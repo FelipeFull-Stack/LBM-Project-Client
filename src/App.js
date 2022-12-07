430b34dffdcc751f47e1c80daa0a442aeacc1637
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-07 19:12:17 -0300

    Signup-Pages - done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthContextComponent } from "./context/authContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
-
+import { Signup } from "./pages/Signup";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <AuthContextComponent>
         <Routes>
           <Route path="/" />
-          <Route path="/" />
+          <Route path="/signup" element={<Signup />} />
           <Route path="/" />
           <Route path="/" element={<ProtectedRoute component={""} />} />
         </Routes>
