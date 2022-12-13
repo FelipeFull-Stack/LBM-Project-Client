27d307498615093e36d95320d7efd2527552c008
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-13 13:25:45 -0300

    CardProcess - done

--- src/components/Cards/CardCustomer/index.js
+++ src/components/Cards/CardCustomer/index.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
 
-function CardCustomer(props) {
+function CardCustomer() {
+
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        cpf: 0,
+        age: 0,
+        phone: 0
+    });
 
     
-
     return (
         <>
 
---
+++ src/components/Cards/CardMeeting/index.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function CardMeeting() {
+
+    const [form, setForm] = useState({
+        date: 0,
+        type: "",
+    });
+
+    
+    return (
+        <>
+
+        </>
+    )
+}
+
+export { CardMeeting }
