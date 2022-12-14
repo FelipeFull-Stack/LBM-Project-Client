379154063d782a6a3730cc2132a47692b89e6dfb
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-14 13:39:25 -0300

    setCustomerContent / setProcessContent / setMeetingContent  =  done

--- src/components/Cards/CardDisplay/index.js
+++ src/components/Cards/CardDisplay/index.js
@@ -1,12 +1,36 @@
 import { api } from "../../../api/api";
 import { useNavigate } from "react-router-dom";
 import { authDisplayContext } from "../../../context/authDisplayContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
-function CardDisplay(props) {
+function CardDisplay() {
 
     const { displaySelect, setDisplaySelect } = useContext(authDisplayContext);
-    const { } = props;
+    const [customerContents, setCustomerContents] = useState([]);
+    const [processContents, setProcessContents] = useState([]);
+    const [meetingContents, setMeetingContents] = useState([]);
+
+    useEffect(() => {
+        async function fetchContents() {
+            try {
+                if(displaySelect.selected === "customer"){
+                    const res = await api.get("/customer");
+                    setCustomerContents(res.data);
+                }
+                if(displaySelect.selected === "process"){
+                    const res = await api.get("/process");
+                    setProcessContents(res.data);
+                }
+                if(displaySelect.selected === "meeting"){
+                    const res = await api.get("/meeting");
+                    setMeetingContents(res.data);
+                }
+            } catch (err) {
+                console.log(`Erro no Front-end - CardDisplay : ${err}`);
+            }
+        }
+        fetchContents();
+    }, [])
 
 
 
