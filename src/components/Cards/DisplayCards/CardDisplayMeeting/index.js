1cc60ccb8e453370fc1ccf272a37b707de448fe8
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-14 20:01:30 -0300

    CardDisplayProcess.css - done

--- src/components/Cards/DisplayCards/CardDisplayMeeting/index.js
+++ src/components/Cards/DisplayCards/CardDisplayMeeting/index.js
@@ -10,6 +10,18 @@ import {
 function CardDisplayMeeting(props) {
     const { date, time, type } = props;
 
+    let stringType = "";
+    if (type === "CONCILIACAO OU MEDIACAO") {
+        stringType = "Conciliação ou Mediação";
+    }
+    if (type === "INSTRUCAO E JULGAMENTO") {
+        stringType = "Instrução e Julgamento";
+    }
+    if (type === "JUSTIFICACAO") {
+        stringType = "Justificação";
+    }
+
+
     return (
         <MDBRow >
             <MDBCol sm='6'>
@@ -19,9 +31,9 @@ function CardDisplayMeeting(props) {
                         {/* <MDBCardTitle>{name}</MDBCardTitle> */}
                         <MDBCardText style={{ display: "flex", flexDirection: "row", gap: "20px", alignItems: "baseline" }}>
                             <input type="radio" />
-                            <div style={{ fontSize: "16px", fontWeight: "700", width: "300px", border: "solid" }}>{date}</div>
+                            <div style={{ fontSize: "16px", fontWeight: "700", width: "300px" }}>{date}</div>
                             <div style={{ fontSize: "16px", fontWeight: "700", width: "150px" }}>{time}</div>
-                            <div style={{ fontSize: "16px", fontWeight: "700", width: "250px" }}>{type}</div>
+                            <div style={{ fontSize: "16px", fontWeight: "700", width: "225px" }}>{stringType}</div>
                         </MDBCardText>
                     </MDBCardBody>
                 </MDBCard>
--- src/components/Cards/DisplayCards/CardDisplayProcess/index.js
+++ src/components/Cards/DisplayCards/CardDisplayProcess/index.js
@@ -10,6 +10,51 @@ import {
 function CardDisplayProcess(props) {
     const { numProcess, type, value, etapa } = props;
 
+    let stringP = numProcess;
+    stringP = stringP.toString();
+    stringP = stringP.split("");
+    const NumProcessStyle = `${stringP[0]}${stringP[1]}${stringP[2]}${stringP[3]}${stringP[4]}${stringP[5]}${stringP[6]}.${stringP[7]}${stringP[8]}.${stringP[9]}${stringP[10]}${stringP[11]}${stringP[12]}.${stringP[13]}.${stringP[14]}${stringP[15]}.${stringP[16]}${stringP[17]}${stringP[18]}${stringP[19]}`;
+
+    let stringType = "";
+
+    if (type === "CONHECIMENTO") {
+        stringType = "Conhecimento";
+    }
+    if (type === "CAUTELAR") {
+        stringType = "Cautelar";
+    }
+    if (type === "EXECUCAO") {
+        stringType = "Execução";
+    }
+
+    const processValue = `R$: ${value},00`;
+
+    let stringEtapa = "";
+
+    if (etapa === "PETICAO INICIAL") {
+        stringEtapa = "Petição Inicial";
+    }
+    if (etapa === "CITACAO") {
+        stringEtapa = "Citação";
+    }
+    if (etapa === "REPLICA") {
+        stringEtapa = "Réplica";
+    }
+    if (etapa === "FASE PROBATORIA") {
+        stringEtapa = "Fase Probatória";
+    }
+    if (etapa === "SENTENCA") {
+        stringEtapa = "Sentença";
+    }
+    if (etapa === "RECURSOS") {
+        stringEtapa = "Recursos";
+    }
+    if (etapa === "CUMPRIMENTO DA SENTENCA") {
+        stringEtapa = "Cumprimento da Sentença";
+    }
+
+
+
     return (
         <MDBRow >
             <MDBCol sm='6'>
@@ -19,10 +64,10 @@ function CardDisplayProcess(props) {
                         {/* <MDBCardTitle>{name}</MDBCardTitle> */}
                         <MDBCardText style={{ display: "flex", flexDirection: "row", gap: "20px", alignItems: "baseline" }}>
                             <input type="radio" />
-                            <div style={{ fontSize: "16px", fontWeight: "700", width: "180px", border: "solid" }}>{numProcess}</div>
-                            <div style={{ fontSize: "16px", fontWeight: "700", width: "120px" }}>{type}</div>
-                            <div style={{ fontSize: "16px", fontWeight: "700", width: "100px" }}>{value}</div>
-                            <div style={{ fontSize: "16px", fontWeight: "700", width: "250px" }}>{etapa}</div>
+                            <div style={{ fontSize: "16px", fontWeight: "700", width: "240px" }}>{NumProcessStyle}</div>
+                            <div style={{ fontSize: "16px", fontWeight: "700", width: "80px" }}>{stringType}</div>
+                            <div style={{ fontSize: "16px", fontWeight: "700", width: "150px" }}>{processValue}</div>
+                            <div style={{ fontSize: "16px", fontWeight: "700", width: "250px" }}>{stringEtapa}</div>
                         </MDBCardText>
                     </MDBCardBody>
                 </MDBCard>
