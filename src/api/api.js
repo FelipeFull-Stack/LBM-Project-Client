d8a859d980c8861d894efdeae55ddf2e88269aef
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2022-12-18 16:18:28 -0300

    testando

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
     development: "http://localhost:4000",
-    production: "https://lbm-adv.netlify.app",
+    production: "https://lbm-adv.netlify.app",//mudei para testar se esta funcionando corretamente
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
